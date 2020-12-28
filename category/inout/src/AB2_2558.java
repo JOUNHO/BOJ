d251f7efa4e816907c58f3ed9ff240a1767ed8d9
jwh7371 <dnsgh9177@naver.com>
2020-12-29 02:13:19 +0900

    Add inout [BOJ]

---
+++ category/inout/src/AB2_2558.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AB2_2558 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		System.out.print(A+B);
+
+	}
+
+}
