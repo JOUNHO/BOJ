da5297206f6a0d08e8cb834c39d0121cae42a6ae
jwh7371 <dnsgh9177@naver.com>
2021-01-04 01:20:53 +0900

    Add DP [BOJ]

--- category/DP/src/_11722.java
+++ category/DP/src/_11722.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class _11055 {
+public class _11722 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,23 +9,20 @@ public class _11055 {
 		int n=sc.nextInt();
 		int a[]=new int[n+1];
 		int dp[]=new int[n+1];
+		
 		for(int i=1;i<n+1;i++) {
 			a[i]=sc.nextInt();
 		}
-
 		for(int i=1;i<n+1;i++) {
-			dp[i]=a[i];
+			dp[i]=1;
 			for(int j=1;j<i;j++) {
-				if(a[j]<a[i]) {
-					dp[i]=Math.max(a[i]+dp[j], dp[i]);
+				if(a[i]<a[j]) {
+					dp[i]=Math.max(dp[i],dp[j]+1);	
 				}
 			}
 		}
-		
 		Arrays.sort(dp);
 		System.out.println(dp[n]);
-
 	}
 
 }
-
